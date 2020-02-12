853686f8e744c8a3c98804d1d3e2116bdf2aa4b5
isabellaliu <isabella@isabella.local>
2020-02-12 12:48:37 -0500

    onlineStore

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express"); //this imports the express package that was installed within your application
+
+const app = express(); // this creates your express app object
+
+const exphbs= require("express-handlebars");
+
+const model=require("./model/products.js");
+
+//This tells express to set up our template engine has handlebars
+app.engine("handlebars",exphbs());
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.get("/", (req,res)=>{
+
+    res.render("home",{
+        title: "Home Page",
+        heading: "Amazon.ca",
+        categories: model.displayCategories(),
+        bestSellers: model.displayBestsellers()
+    });
+
+});
+
+app.get("/products", (req,res)=>{
+    res.render("products",{
+        title: "Products Page",
+        heading: "Amazon.ca",
+        list: model.displayProductList()
+    });
+});
+
+app.get("/customerRegistration", (req,res)=>{
+    res.render("customerRegistration",{
+        title: "Customer Page",
+        heading: "Amazon.ca"
+    });
+});
+
+app.get("/login", (req,res)=>{
+    res.render("login",{
+        title: "Login Page",
+        heading: "Amazon.ca"
+    });
+});
+
+const PORT = 3000;
+app.listen(PORT, ()=>{
+    console.log("Server is running now!");
+})
