e935a217eeaab7f14c6d519b03492ca8af1166bf
isabellaliu <isabellaliu77@gmail.com>
2020-02-28 17:45:35 -0500

    registration form validation added

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const express = require("express"); //this imports the express package that was installed within your application
 
+
+const exphbs = require("express-handlebars");
+
+const model = require("./model/products.js");
+
+const bodyParser = require('body-parser');
+require('dotenv').config({path:"./config/keys.env"});//load the environment variable file
+
 const app = express(); // this creates your express app object
-
-const exphbs= require("express-handlebars");
-
-const model=require("./model/products.js");
-
-const bodyParser=require('body-parser');
-
 //This tells express to set up our template engine has handlebars
-app.engine("handlebars",exphbs());
+app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get("/", (req,res)=>{
+//const generalController=require("./controller/general");
+//const 
 
-    res.render("home",{
+app.get("/", (req, res) => {
+
+    res.render("home", {
         title: "Home Page",
         heading: "Amazon.ca",
         categories: model.displayCategories(),
@@ -27,40 +31,100 @@ app.get("/", (req,res)=>{
 
 });
 
-app.get("/products", (req,res)=>{
-    res.render("products",{
+app.get("/products", (req, res) => {
+    res.render("products", {
         title: "Products Page",
         heading: "Amazon.ca",
         list: model.displayProductList()
     });
 });
 
-app.get("/customerRegistration", (req,res)=>{
-    res.render("customerRegistration",{
-        title: "Customer Page",
+app.get("/customerRegistration", (req, res) => {
+    res.render("customerRegistration", {
+        title: "Customer Registration Page",
         heading: "Amazon.ca"
     });
 });
+app.post("/customerRegistration", (req, res) => {
+    let errorFName = "", errorLName = "", errorE = "", errorP = "", errorPP = "";
+    if (req.body.firstName == "") {
+        errorFName = "! Please enter your First Name";
+    }
+    else {
+        let valid = false;
+        for (let i = 0; i < req.body.firstName.length; i++) {
+            if ((req.body.firstName[i] >= 'A' && req.body.firstName[i] <= 'Z') || (req.body.firstName[i] >= 'a' && req.body.firstName[i] <= 'z')) {
+                valid = true;
+            }
+            else { valid = false; }
+        }
+        if (!valid) {
+            errorFName = "! First Name should only contain letters, please try again";
+        }
+    }
+    if (req.body.lastName == "") {
+        errorLName = "! Please enter your Last Name";
+    }
+    else {
+        let valid = false;
+        for (let i = 0; i < req.body.lastName.length; i++) {
+            if ((req.body.lastName[i] >= 'A' && req.body.lastName[i] <= 'Z') || (req.body.lastName[i] >= 'a' && req.body.firstName[i] <= 'z')) {
+                valid = true;
+            }
+            else { valid = false; }
+        }
+        if (!valid) {
+            errorLName = "! Last Name should only contain letters, please try again";
+        }
+    }
+    if (req.body.email == "") {
+        errorE = "! Please enter your Email Address";
+    }
+    if (req.body.password.length < 6 || req.body.password > 12) {
+        errorP = "! The password must be 6 to 12 characters";
+    }
+    else if (req.body.password != req.body.passwordAgain) {
+        errorPP = "! The password entered does not match, please try again";
+    }
 
-app.get("/login", (req,res)=>{
-    res.render("login",{
+
+    if (errorFName != "" || errorLName != "" || errorE != "" || errorP != "" || errorPP != "") {
+        res.render("customerRegistration", {
+            title: "Customer Registration Page",
+            heading: "Amazon.ca",
+            errorFName: errorFName,
+            errorLName: errorLName,
+            errorE: errorE,
+            errorP: errorP,
+            errorPP: errorPP
+        });
+    }
+    else {
+        res.redirect("/");
+    }
+
+});
+
+
+app.get("/login", (req, res) => {
+    res.render("login", {
         title: "Login Page",
         heading: "Amazon.ca"
     });
 });
 
-app.post("/login", (req,res)=>{
-    let errorE=[];let errorP=[];
-    if(req.body.email==""){
-        errorE.push("! Please enter a valid email");
+app.post("/login", (req, res) => {
+    let errorE = "", errorP = "";
+    if (req.body.email == "") {
+        errorE = "! Please enter a valid email";
     }
-    if(req.body.password==""){
-        errorP.push("! Please enter your password");
+    if (req.body.password == "") {
+        errorP = "! Please enter your password";
     }
 
-    if ((errorE!="") || (errorP!="")){
-      res.render("login",{
-            title:"Login Page",
+    if ((errorE != "") || (errorP != "")) {
+        res.render("login", {
+            title: "Login Page",
             heading: "Amazon.ca",
             errorE: errorE,
             errorP: errorP
@@ -72,7 +136,7 @@ app.post("/login", (req,res)=>{
 
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
     console.log("Server is running now!");
 })
